534f8615e24433a1acc7e49c4a2dcb8d41758da7
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2022-10-19 10:49:56 +0200

    add CSS, dark mode toggle, most questions to an array

--- main.js
+++ main.js
@@ -1,34 +1,4 @@
 /*
-Are there hedgehogs at the Urban Rescue Ranch?
-A. Yes B. No
-
-Are there cats at the Urban Rescue Ranch?
-A. Yes B. No
-
-Are there horses at the Urban Rescue Ranch?
-A. Yes B. No
-
-Are there pigs at the Urban Rescue Ranch?
-A. Yes B. No
-
-Are there kangaroos at the Urban Rescue Ranch?
-A. Yes B. No
-
-Is the owner of the Urban Rescue Ranch called Ben?
-A. Yes B. No
-
-Are the animals cute?
-A. Yes B. No
-
-Has any animal ever escaped?
-A. Yes B. No
-
-Is it possible to catch a goose and bring it home with you?
-A. Yes B. No
-
-Is the Urban Rescue Ranch full now?
-A. Yes B. No
-
 Select the ratite species present at the Urban Rescue Ranch.
 -Ostrich -Emu -Rhea -Kiwi 
 
@@ -46,12 +16,89 @@ A. To live a peaceful and happy life B. To run from the edge to the other edge i
 
 */
 
+// multipleChoice
+// checkboxes
+
+
 const questions = [
   {
     questionType: "trueFalse",
-    // multipleChoice
-    // checkboxes
-    question: "",
-    answers: "",
+    question: "Are there hedgehogs at the Urban Rescue Ranch?",
+    answers: "Yes, No",
+  },
+  {
+    questionType: "trueFalse",
+    question: "Are there cats at the Urban Rescue Ranch?",
+    answers: "Yes, No",
+  },
+  {
+    questionType: "trueFalse",
+    question: "Are there horses at the Urban Rescue Ranch?",
+    answers: "Yes, No",
+  },
+  {
+    questionType: "trueFalse",
+    question: "Are there pigs at the Urban Rescue Ranch??",
+    answers: "Yes, No",
+  },
+  {
+    questionType: "trueFalse",
+    question: "Are there kangaroos at the Urban Rescue RAre there hedgehogs at the Urban Rescue Ranch?",
+    answers: "Yes, No",
+  },
+  {
+    questionType: "trueFalse",
+    question: "Is the owner of the Urban Rescue Ranch called Ben?",
+    answers: "Yes, No",
+  },
+  {
+    questionType: "trueFalse",
+    question: "Are the animals cute?",
+    answers: "Yes, No",
+  },
+  {
+    questionType: "trueFalse",
+    question: "Has any animal ever escaped?",
+    answers: "Yes, No",
+  },
+  {
+    questionType: "trueFalse",
+    question: "Is it possible to catch a goose and bring it home with you?",
+    answers: "Yes, No",
+  },
+  {
+    questionType: "trueFalse",
+    question: "Is the Urban Rescue Ranch full now?",
+    answers: "Yes, No",
   },
 ];
+
+let mainDiv = document.createElement("div");
+document.body.append(mainDiv);
+let darkModeToggleButton = document.createElement("button");
+darkModeToggleButton.innerText = "Toggle Dark Mode";
+darkModeToggleButton.addEventListener("click", () => { toggleDarkMode(); });
+let darkMode = false;
+mainDiv.append(darkModeToggleButton);
+
+function toggleDarkMode() {
+  if (darkMode) {
+    document.body.className = "light";
+    darkModeToggleButton.className = "light-button";
+    darkModeToggleButton.innerText = "Toggle Dark Mode";
+    darkMode = false;
+  }
+  else {
+
+    document.body.className = "dark";
+    darkModeToggleButton.className = "dark-button";
+
+    darkModeToggleButton.innerText = "Toggle Light Mode";
+    darkMode = true;
+  }
+}
+
+function displayQuestion() {
+
+}
+
--- readme.md
+++ readme.md
@@ -14,7 +14,7 @@ Du ska även ha ditt projekt på Github och pusha upp kod kontinuerligt under pr
 
 ?- Quizet ska innehålla 10 frågor. Frågorna ska bestå utav sant/falskt påståenden med två möjliga svarsalternativ: Sant eller Falskt.
 
-?- Användaren ska utöver att besvara frågor för quizet, även kunna byta utseende på sidan mellan dark mode (dvs mörk bakgrundsfärg med ljus text ) och light mode (ljus bakgrundsfärg med mörk text).
+*- Användaren ska utöver att besvara frågor för quizet, även kunna byta utseende på sidan mellan dark mode (dvs mörk bakgrundsfärg med ljus text ) och light mode (ljus bakgrundsfärg med mörk text).
 
 ?- Användaren ska kunna besvara samtliga frågor och sedan klicka på en knapp för att se sitt resultat.
 
