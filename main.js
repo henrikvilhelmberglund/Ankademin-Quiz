fb5abe1e837ec74dccf3835bd1070f4272448cb2
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2022-10-19 10:55:35 +0200

    add the rest of the questions

--- main.js
+++ main.js
@@ -1,18 +1,4 @@
 /*
-Select the ratite species present at the Urban Rescue Ranch.
--Ostrich -Emu -Rhea -Kiwi 
-
-Select the flying bird species present at the Urban Rescue Ranch.
--Call duck -Hawk -Goose -Flamingo
-
-What was at the location before it became the Urban Rescue Ranch?
-A. A field B. A gas station C. A crackhouse  D. A farm
-
-What does the owner of the Urban Rescue Ranch like to add before his name?
-A. Farmer B. Mister C. Ranger D. Uncle
-
-What is the goal the owner of the Urban Rescue Ranch has for his animal friend DaBaby?
-A. To live a peaceful and happy life B. To run from the edge to the other edge in 10 seconds C. To fight Jake Paul in a boxing match  D. To win a prize in an exhibition
 
 */
 
@@ -70,6 +56,31 @@ const questions = [
     questionType: "trueFalse",
     question: "Is the Urban Rescue Ranch full now?",
     answers: "Yes, No",
+  },
+  {
+    questionType: "checkboxes",
+    question: "Select the ratite species present at the Urban Rescue Ranch.",
+    answers: "Ostrich, Emu, Rhea, Kiwi",
+  },
+  {
+    questionType: "checkboxes",
+    question: "Select the flying bird species present at the Urban Rescue Ranch.",
+    answers: "Call duck, Hawk, Goose, Flamingo",
+  },
+  {
+    questionType: "multipleChoice",
+    question: "What was at the location before it became the Urban Rescue Ranch?",
+    answers: "A field, A gas station, A crackhouse, A farm",
+  },
+  {
+    questionType: "multipleChoice",
+    question: "What does the owner of the Urban Rescue Ranch like to add before his name?",
+    answers: "Farmer, Mister, Ranger, Uncle",
+  },
+  {
+    questionType: "multipleChoice",
+    question: "hat is the goal the owner of the Urban Rescue Ranch has for his animal friend DaBaby?",
+    answers: "To live a peaceful and happy life, To run from the edge to the other edge of the ranch in 10 seconds, To fight Jake Paul in a boxing match, To win a prize in an exhibition"
   },
 ];
 
